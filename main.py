7f8113f6d327d2f13fe216beb8fce624c44bdfca
Renzhentaxi Baerde <Renzhentaxibaerde@gmail.com>
2015-08-21 16:19:35 -0400

    Added basic GUI

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
-#!/usr/bin/python
+import pygame,sys
 
 
-#----------- Imports ---------------
-#'import pygame' or 'from pygame import ....'
-#import filename if you're making multiple scripts.
+gameSize = 9
+screenWidth = 800
+screenHeight = 600
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+boardSize = screenHeight
+unitSize = int(boardSize/gameSize)
+#display
+pygame.init()
+screen = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.display.set_caption("GO by Taxi")
+#classes
+class Intersection:
+    intersections = []
+    def __init__(self,x,y):
+        self.intersections.append(self)
+        self.x = x
+        self.y = y
+    def draw(self,surface):
+        pygame.draw.circle(surface,BLACK,((self.x+1)*unitSize,(self.y+1)*unitSize),int(unitSize/2))
+#functions
+def start():
+    #createIntersections
+    Intersection.intersections = []
+    
+    for x in range(gameSize-1):
+        for y in range(gameSize-1):
+            Intersection(x,y)
 
+def drawBoard(surface):
+  surface.fill(WHITE)
+  for x in range(gameSize):
+    for y in range(gameSize):
+      pygame.draw.line(surface,BLACK,(x*unitSize,0),(x*unitSize,boardSize))
+      pygame.draw.line(surface,BLACK,(0,y*unitSize),(boardSize,y*unitSize))
+  for i in Intersection.intersections:
+      i.draw(surface)
 
-#---------- Game Loop --------------
-def main():  # might need args
-  '''Docstring for function... Something like runs all functions
-  checks for valid moves, checks to see if player surrenders 
-  ends game when criteria is met etc.'''
-  
-  
-#---------- Running the File -------
-if __name__ == '__main__':
-  main()
+def mouseClicked(pos):
+    a = 4
+start()
+#loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseClicked(event.pos)
+        drawBoard(screen)
+        pygame.display.flip()
