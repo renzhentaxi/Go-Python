d6baa43af8cbc21db2cbe441d60d0a3319caa396
Dor-Ron <dor.rondel@gmail.com>
2015-08-23 13:18:19 -0400

    combined bilt and pixel to cood branch, made multiple surfaces, mouse and drawwork properly

--- main.py
+++ main.py
@@ -1,42 +1,84 @@
-#----------- Imports ------------------------------
+#----------Imports-------------------------------
+
 
 import pygame
 import sys
 
 
-#---------- Board Setup ---------------------------
+#---------Board setup---------------------------
 
 
-BLACK = (0,0,0)
-WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 board_color = (161, 148, 36)
 
-game_size = 10 
-screen_width = 800
-screen_height = 800
+game_size = 10
+screen_width = 860
+screen_height = 640
 board_size = screen_height - 100  
-unit_size = int(board_size /game_size)
-unit_sizeh = int(unit_size/2)
-offsetx = (screen_width-board_size)/2
-offsety = (screen_height-board_size)/2
-board_surface = pygame.Surface((screen_width, screen_height))
+unit_size = int(board_size / game_size)
+unit_sizeh = int(unit_size / 2)
+offsetx = (screen_width - board_size) / 2
+offsety = (screen_height - board_size) / 2
 board_surface = pygame.Surface((board_size, board_size))
-white_surface = pygame.Surface((unit_size, unit_size))
-black_surface = pygame.Surface((unit_size, unit_size))
 
-#---------- PyGame Config -------------------------
+
+#-------------- pygame config ---------------------------
 
 
 pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("GO by Taxi")
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("GO by Taxi and Dor")
+
+def draw_init():
+    '''creates a surface for the board'''
+    board_surface.fill(board_color)
+
+    for x in range(1, game_size):
+        for y in range(1, game_size):
+            pygame.draw.line(board_surface,BLACK,(x*unit_size,unit_size),(x*unit_size,unit_size*(game_size-1)),3)
+            pygame.draw.line(board_surface,BLACK,(unit_size,y*unit_size),(unit_size*(game_size-1),y*unit_size),3)
 
 
 
-#--------- Board & Pieces Functions ---------------
+#----------------- Game Functions ------------------------
+
+def mouse_clicked(event):
+    '''Checks mouse coordinates, prints to terminal'''
+    print(pos2grid(event.pos))
+    #dd_stone(white_dict,"white",pos2grid(event.pos))
 
 
-def make_board_tuple(num):
+def pos2grid(pos):
+    '''Converts pixel location to board coordinates'''
+    x = pos[0] - offsetx - unit_sizeh
+    y = pos[1] - offsety-unit_sizeh
+    x = (x - x % unit_size) / unit_size
+    y = (y - y % unit_size) / unit_size
+    return (x,y)
+
+
+def grid2pos(grid):
+    '''converts grid position to pixel location'''
+    x = (grid[0]*unit_size)+offsetx+unit_size
+    y = grid[1]*unit_size+offsety+unit_size
+    return (x,y)
+
+
+def draw():
+    '''draws board, pygame'''
+    screen.fill(WHITE)
+    screen.blit(board_surface,(offsetx,offsety))
+    for val in black_dict.values():
+        for tupl in val:
+            draw_stone(screen, tupl[0], tupl[1], BLACK)
+
+    for val in white_dict.values():
+        for tupl in val:
+            draw_stone(screen, tupl[0], tupl[1], WHITE)
+
+
+def make_board_tuple():
     '''Makes a list of tuples to range num exclusive. of form (x,y)
     eg) [
           (0, 0), (1, 0), (2, 0),
@@ -44,35 +86,18 @@ def make_board_tuple(num):
           (2, 0), (2, 1), (2, 2)
         ]
     '''
-    return [(j, i) for i in range(num) for j in range(num)]
-
-def draw_init():
-    #init board surface
-    board_surface.fill(board_color)
-
-    for x in range(1,game_size):
-        for y in range(1,game_size):
-            pygame.draw.line(board_surface,BLACK,(x*unit_size,unit_size),(x*unit_size,unit_size*9),3)
-            pygame.draw.line(board_surface,BLACK,(unit_size,y*unit_size),(unit_size*9,y*unit_size),3)
-    #init stone surface
-    rect =white_surface.get_rect()
-    pygame.draw.circle(white_surface,WHITE,rect.center,unit_sizeh)
-    pygame.draw.circle(black_surface,BLACK,rect.center,unit_sizeh)
+    return [(j, i) for i in range(game_size-1) for j in range(game_size-1)]
 
 
-def draw_black(surface, x, y):
+
+def draw_stone(surface, x, y,color):
     '''draws a black stone'''
-    pygame.draw.circle(surface, BLACK,((x+1)*unit_size,(y+1)*unit_size), int(unit_size/2))
+    x, y = grid2pos((x,y))
+    pygame.draw.circle(surface, color, (x, y), unit_sizeh)
 
 
-def draw_white(surface, x, y):
-    '''draws a white circle'''
-    pygame.draw.circle(surface, WHITE,((x+1)*unit_size,(y+1)*unit_size), int(unit_size/2))
 
-
-#---------------- Game Functions -------------------
-
-board_tuples = make_board_tuple(9)
+board_tuples = make_board_tuple()
 
 empty_dict = {
                 "empty": board_tuples
@@ -86,8 +111,10 @@ black_dict = {
                 "black": []
              }
 
+
 white_kills = 0
 black_kills = 0
+
 
 def add_stone(dictionary, key, coor):
     '''Adds tuple stone to dictionary'''
@@ -109,47 +136,27 @@ def remove_stone(dictionary, key, coor):
     dictionary[str(key)].remove(coor)
 
 
-
-#check if move valid
-#like def count_score (+= kill_count)
-#def komi_count
-#def atari_check
-#def ko_check
-
-
-#---------- Event Handling Functions ---------------
-
-
-def mouse_clicked(event):
-    x = event.pos[0]-offsetx-unit_sizeh
-    y = event.pos[1]-offsety-unit_sizeh
-    x = (x-x%unit_size)/unit_size
-    y = (y-y%unit_size)/unit_size
-    print(x,y)
-
-
-
-#Should go under Game Functions section
 def game_loop():
-    '''Infinite game loop until broken'''
+    '''infinite game loop, until broken'''
+    draw_init()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_clicked(event.pos)
-            pygame.display.flip()
+                mouse_clicked(event)
+            draw()
+        pygame.display.flip()
 
 
-#------------------ Tests -------------------------  # Delete before release  
+#-------------------- Tests -------------------------
 
-# Fill with some tests to make sure everything is working
 
 #black pieces, should only be at (3, 5) and (0, 1)
 add_stone(black_dict, 'black', (3, 5))
 add_stone(black_dict, 'black', (8, 8))
-add_stone(black_dict, 'black', (0, 1))
+add_stone(black_dict, 'black', (0, 0))
 remove_stone(black_dict, 'black', (8, 8))
 print("Total white kills: {}".format(white_kills))  # should be one, only one removed
 
@@ -166,19 +173,8 @@ remove_stone(white_dict, 'white', (7, 6))
 print("Total black kills: {}".format(black_kills))  # Should be 2 since white got removed twice
 
 
-#Atari and random black piece scenario
-draw_board(screen)
 
-for val in black_dict.values():
-    for tupl in val:
-        draw_black(screen, tupl[0], tupl[1])
-
-for val in white_dict.values():
-    for tupl in val:
-        draw_white(screen, tupl[0], tupl[1])
-
-
-#---------------- Running Game --------------------
+#-------------------- Running Game ---------------------
 
 
 if __name__ == '__main__':
